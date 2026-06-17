dbc963fb2494f5881a21ab5dcf9375218ab2033b
ragnarokbrasil123-del <ragnarokbrasil123@gmail.com>
2026-06-17 18:44:52 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
